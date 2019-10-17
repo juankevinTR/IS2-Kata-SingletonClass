6aa7c3ce47c4fe755f993bc8de15c2d74752aa3a
Juan Kevin Trujillo <juankevin.tr@gmail.com>
2019-10-17 13:29:21 +0100

    Adding threads!

---
+++ src/com/juankevintrujillo/IThread.java
@@ -0,0 +1,25 @@
+package com.juankevintrujillo;
+
+/**
+ *
+ * @author juankevintr
+ */
+public class IThread extends Thread {
+    
+    int ID;
+    SingletonClass INSTANCE;
+    
+    public IThread (int p_ID){
+        this.ID = p_ID;
+    }
+    
+    @Override
+    public void run(){
+        INSTANCE = SingletonClass.getInstance();
+    }
+    
+    int getHashCode(){
+        return INSTANCE.hashCode();
+    }
+    
+}
--- src/com/juankevintrujillo/Main.java
+++ src/com/juankevintrujillo/Main.java
@@ -1,5 +1,7 @@
 package com.juankevintrujillo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author juankevintr
@@ -7,13 +9,31 @@ package com.juankevintrujillo;
 public class Main {
 
     public static void main(String[] args) {
-        
-        SingletonClass my_SC_0 = SingletonClass.getInstance();
-        SingletonClass my_SC_1 = SingletonClass.getInstance();
-        
-        System.out.println("HasCode my_SC_0: " + my_SC_0.hashCode());
-        System.out.println("HasCode my_SC_1: " + my_SC_1.hashCode());
-        
+
+        // Version 1
+        /*
+            SingletonClass my_SC_0 = SingletonClass.getInstance();
+            SingletonClass my_SC_1 = SingletonClass.getInstance();
+
+            System.out.println("HasCode my_SC_0: " + my_SC_0.hashCode());
+            System.out.println("HasCode my_SC_1: " + my_SC_1.hashCode());
+         */
+        // Version 2
+        int number_threads = 10;
+        ArrayList<IThread> my_threads = new ArrayList<>();
+
+        for (int v_i = 0; v_i < number_threads; v_i++) {
+            my_threads.add(new IThread(v_i));
+        }
+
+        for (IThread my_thread : my_threads) {
+            my_thread.start();
+        }
+
+        for (IThread my_thread : my_threads) {
+            System.out.println("[" + my_thread.ID + "]:" + my_thread.getHashCode());
+        }
+
     }
-    
+
 }
